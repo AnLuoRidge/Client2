8a3b891731b0c303b4141aa6b5de7a03b1d77d0f
Yupeili <474680647@qq.com>
2018-10-05 22:27:03 +0800

    update url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    DEV_API_BASE_URL: 'http://localhost:5000/api',
-    DEV_UPLOAD_BASE_URL: 'http://localhost:5000',
-    API_BASE_URL: '',
-    UPLOAD_BASE_URL: '',
+	DEV_API_BASE_URL: 'https://lovely-aip.herokuapp.com/api',
+	DEV_UPLOAD_BASE_URL: 'https://lovely-aip.herokuapp.com',
+	API_BASE_URL: '',
+	UPLOAD_BASE_URL: '',
 };
 
 export default config;
